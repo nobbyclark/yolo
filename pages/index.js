d41c838cf7dc1795fc4ee71412226e5c3f64414b
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-07 21:44:22 +1300

    Update with new folder structure

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,17 @@
-import Layout from '../components/MyLayout.js'
+import React from 'react'
 import Link from 'next/link'
 
-const PostLink = props => (
-  <li>
-    <Link href="/p/[id]" as={`/p/${props.id}`}>
-      <a>{props.id}</a>
-    </Link>
-  </li>
+export default () => (
+  <ul>
+    <li>
+      <Link href="/b" as="/a">
+        <a>a</a>
+      </Link>
+    </li>
+    <li>
+      <Link href="/a" as="/b">
+        <a>b</a>
+      </Link>
+    </li>
+  </ul>
 )
-
-export default function Blog() {
-  return (
-    <Layout>
-      <h1>My Blog</h1>
-      <ul>
-        <PostLink id="hello-nextjs" />
-        <PostLink id="learn-nextjs" />
-        <PostLink id="deploy-nextjs" />
-      </ul>
-    </Layout>
-  )
-}
