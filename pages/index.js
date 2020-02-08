ca13a3eb192b56070313d1842ba68c183c3632b2
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-09 12:48:20 +1300

    Add custom key env

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,13 @@
 import React from 'react'
 import Link from 'next/link'
-import getConfig from 'next/config'
-
-const { publicRuntimeConfig } = getConfig()
-const { API_URL } = publicRuntimeConfig
 
 export default () => (
   <ul>
     <li>
       <Link href="/b" as="/a">
-        <a>{API_URL}</a>
+        <a>{process.env.customKey}</a>
       </Link>
     </li>
-    <li>{process.env.API_URL}</li>
     <li>
       <Link href="/a" as="/b">
         <a>b</a>
