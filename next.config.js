12f4340384a8c29f57dd715f6fcb08a6187279f7
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-09 15:29:08 +1300

    try more shit

--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 module.exports = {
-  serverRuntimeConfig: {
-    the_api_url_server_runtime: process.env.API_URL
-  },
-  publicRuntimeConfig: {
-    the_api_url: process.env.API_URL
+  env: {
+    API_URL: process.env.API_URL
   }
 };
