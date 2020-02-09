e19d3d5b2f9afa7520b3a40e005d29e0c1686dd6
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-09 15:34:15 +1300

    Fix some shit

--- pages/index.js
+++ pages/index.js
@@ -1,19 +1,11 @@
 import React from 'react'
 import Link from 'next/link'
-import getConfig from 'next/config'
-
-// Only holds serverRuntimeConfig and publicRuntimeConfig
-const { serverRuntimeConfig, publicRuntimeConfig } = getConfig()
-// Will only be available on the server-side
-console.log(serverRuntimeConfig.the_api_url_server_runtime)
-// Will be available on both server-side and client-side
-console.log(publicRuntimeConfig.the_api_url)
 
 export default () => (
   <ul>
     <li>
       <Link href="/b" as="/a">
-        <a>{publicRuntimeConfig.the_api_url}</a>
+        <a>a</a>
       </Link>
     </li>
     <li>
