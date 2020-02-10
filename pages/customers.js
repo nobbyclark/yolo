0059c519edd7a12fe61d2b6c4f9e3b4893f7a1c8
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-10 16:01:46 +1300

    Update

--- pages/customers.js
+++ pages/customers.js
@@ -1,9 +1,8 @@
 import fetch from "isomorphic-unfetch";
 
-CustomersPage.getInitialProps = async ctx => {
-  console.log(Object.keys(ctx.req))
-  console.log(ctx.req.headers)
-  const pageRequest = `https://yolonextjs.azurewebsites.net/api/customers`;
+CustomersPage.getInitialProps = async ({ req }) => {
+  const { headers } = req;
+  const pageRequest = `${headers['x-forwarded-proto']}://${headers.host}/api/customers`;
   const res = await fetch(pageRequest);
   const json = await res.json();
   return json;
