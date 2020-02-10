db669644edb1dea2eb8d8c70949947ba43f933a7
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-10 15:19:58 +1300

    Fix spelling mistake

--- pages/customers.js
+++ pages/customers.js
@@ -2,7 +2,7 @@ import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 
 CustomersPage.getInitialProps = async ({ req, query }) => {
-  const protocol = req.headers.referer.split('://')[0];
+  const protocol = req.headers.referrer.split('://')[0];
   const host = req.header.host;
   const pageRequest = `${protocol}//${host}/api/customers`;
   const res = await fetch(pageRequest);
