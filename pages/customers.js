9dbb8eb36d7c249cb6c2796f1471e63573819da3
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-10 15:12:09 +1300

    Fix protocol and host

--- pages/customers.js
+++ pages/customers.js
@@ -2,10 +2,8 @@ import fetch from "isomorphic-unfetch";
 import Link from "next/link";
 
 CustomersPage.getInitialProps = async ({ req, query }) => {
-  const protocol = req
-    ? `${req.headers["x-forwarded-proto"]}:`
-    : location.protocol;
-  const host = req ? req.headers["x-forwarded-host"] : location.host;
+  const protocol = req.headers.referer.split('://')[0];
+  const host = req.header.host;
   const pageRequest = `${protocol}//${host}/api/customers`;
   const res = await fetch(pageRequest);
   const json = await res.json();
