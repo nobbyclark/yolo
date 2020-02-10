af7c6b204a4d062a2a6319e1efb36391903defb8
Nick Clark <nick.clark@dovetailstudios.com>
2020-02-10 15:50:15 +1300

    Add more debug

--- pages/customers.js
+++ pages/customers.js
@@ -1,8 +1,8 @@
 import fetch from "isomorphic-unfetch";
-import Link from "next/link";
 
 CustomersPage.getInitialProps = async ctx => {
-  console.log(Object.keys(ctx))
+  console.log(Object.keys(ctx.req))
+  console.log(ctx.req.headers)
   const pageRequest = `https://yolonextjs.azurewebsites.net/api/customers`;
   const res = await fetch(pageRequest);
   const json = await res.json();
